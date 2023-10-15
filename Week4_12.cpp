8c189c94d2e77b608b3b4f98fd0e658ef150ee45
BhavaniSreeA <143406233+BhavaniSreeA@users.noreply.github.com>
2023-10-15 16:37:24 +0530

    Update Week4_12.cpp

--- Week4_12.cpp
+++ Week4_12.cpp
@@ -4,13 +4,13 @@ int main()
 	int n,i,j;
 	printf("Enter number of rows");
 	scanf("%d",&n);
-	for(i=0;i<=n;i++)
+	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n-i;j++)
 			printf("*");
 		printf("\n");
 	}
-	for(i=0;i<=n;i++)
+	for(i=0;i<n;i++)
 	{
 		for(j=0;j<i+1;j++)
 			printf("*");
