320eea331e4aa848e9cec0a2b24e27fabb3cd3f0
BhavaniSreeA <143406233+BhavaniSreeA@users.noreply.github.com>
2023-11-03 18:33:04 +0530

    Update Week5_add_2_matrix.cpp

--- Week5_add_2_matrix.cpp
+++ Week5_add_2_matrix.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	int n,i,j;
-	int a[n][n],b[n][n],c[n][n];
+	int a[n][n],b[n][n],c[n][n],d[n][n];
 	printf("Enter number of rows");
 	scanf("%d",&n);
 	printf("Enter values for %d*%d Matrix 1",n,n);
@@ -20,12 +20,23 @@ int main()
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
-		c[i][j]=a[i][j]+b[i][j];
+		{	
+			c[i][j]=a[i][j]+b[i][j];
+			d[i][j]=a[i][j]-b[i][j];
+		}
 	}
+	printf("Sum of two matrices is");
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
 		printf("%d ",c[i][j]);
+		printf("\n");
+	}
+	printf("Difference of two matrices is");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		printf("%d ",d[i][j]);
 		printf("\n");
 	}
 	return 0;
