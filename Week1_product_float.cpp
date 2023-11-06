99facace048852af749785ecd7089f5ff15bef3b
BhavaniSreeA <143406233+BhavaniSreeA@users.noreply.github.com>
2023-11-06 18:37:59 +0530

    Update Week1_product_float.cpp

--- Week1_product_float.cpp
+++ Week1_product_float.cpp
@@ -7,5 +7,5 @@ scanf("%f%f",&a,&b);
 float c;
 c=a*b;
 printf("The product of two numbers in float form is %f \n",c);
-printf("The product of two numbers in integer form is %d",c);
+printf("The product of two numbers in integer form is %d",int(c));
 }
