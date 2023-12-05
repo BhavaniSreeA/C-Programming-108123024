22ea2da7d712ea802e4ed243185d8885f3c21ec1
BhavaniSreeA <143406233+BhavaniSreeA@users.noreply.github.com>
2023-12-05 08:50:30 +0530

    Update Week2_Vowel.cpp

--- Week2_Vowel.cpp
+++ Week2_Vowel.cpp
@@ -4,15 +4,7 @@ int main()
 char ch;
 printf("Enter a character(a-z)");
 scanf("%c",&ch);
-if (ch=='a')
-printf("The character is a vowel");
-else if (ch=='e')
-printf("The character is a vowel");
-else if (ch=='i')
-printf("The character is a vowel");
-else if (ch=='o')
-printf("The character is a vowel");
-else if (ch=='u')
+if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
 printf("The character is a vowel");
 else
 printf("The character is a consonant");
